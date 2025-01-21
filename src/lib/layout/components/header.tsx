3cb45583b3297b9d436857f4469bbada9bb81421
Bennett Dungan <bennettdungan@gmail.com>
2025-01-20 20:37:56 -0500

    revert: broken link highlights

--- src/lib/layout/components/header.tsx
+++ src/lib/layout/components/header.tsx
@@ -1,19 +1,9 @@
 import { Flex, HStack, Heading, Link } from '@chakra-ui/react';
-import { useLocation } from 'react-router';
 import { LeftFlipper } from './left-flipper';
 import { Demo } from './mobile-drawer';
 import { RightFlipper } from './right-flipper';
 
-const TRAILING_SLASH_REGEX = /\/$/;
-
 export const Header = () => {
-  const location = useLocation();
-  const currentPath = location.pathname;
-
-  const isActive = (path: string) =>
-    currentPath.replace(TRAILING_SLASH_REGEX, '') ===
-    path.replace(TRAILING_SLASH_REGEX, '');
-
   return (
     <Flex as="header" width="full" align="center" direction="column" rowGap={4}>
       <HStack>
@@ -43,32 +33,16 @@ export const Header = () => {
       </Flex>
 
       <Flex gap={6} display={{ base: 'none', md: 'flex' }}>
-        <Link
-          href="/"
-          fontWeight={isActive('/') ? 'bold' : 'normal'}
-          _hover={{ textDecoration: 'underline' }}
-        >
+        <Link href="/" _hover={{ textDecoration: 'underline' }}>
           Home
         </Link>
-        <Link
-          href="/new-players"
-          fontWeight={isActive('/new-players') ? 'bold' : 'normal'}
-          _hover={{ textDecoration: 'underline' }}
-        >
+        <Link href="/new-players" _hover={{ textDecoration: 'underline' }}>
           New Players Guide
         </Link>
-        <Link
-          href="/resources"
-          fontWeight={isActive('/resources') ? 'bold' : 'normal'}
-          _hover={{ textDecoration: 'underline' }}
-        >
+        <Link href="/resources" _hover={{ textDecoration: 'underline' }}>
           Resources
         </Link>
-        <Link
-          href="/about"
-          fontWeight={isActive('/about') ? 'bold' : 'normal'}
-          _hover={{ textDecoration: 'underline' }}
-        >
+        <Link href="/about" _hover={{ textDecoration: 'underline' }}>
           About
         </Link>
       </Flex>
