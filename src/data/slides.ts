95f73df4fe44ba6b4d112e0172bec8dae5be886e
Bennett Dungan <bennettdungan@gmail.com>
2025-05-18 21:31:27 -0500

    feat: added photos

--- src/data/slides.ts
+++ src/data/slides.ts
@@ -1,4 +1,7 @@
 export const slides = [
+  { image: 'bt_group_photo_optimized.jpg' },
+  { image: 'group_playing_optimized.jpg' },
+  { image: 'nick_alan_ariel_optimized.jpg' },
   { image: 'brcade_russel_optimized.jpg' },
   { image: 'bt_group_optimized.jpg' },
   { image: 'bt_winners_optimized.jpg' },
